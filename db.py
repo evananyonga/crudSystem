3e7a3d0ad9545e98f92ad07539bd59cb7dd29b43
evananyonga <evewish@gmail.com>
2021-04-27 04:36:54 +0300

    add database connection

---
+++ db.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+# connect to the database
+con = psycopg2.connect(
+    host = "localhost",
+    database = "blogdb",
+    user = "postgres",
+    password = "@llahu",)
+
+# close the database
+con.close()
